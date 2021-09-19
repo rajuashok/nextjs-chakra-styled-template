b62d379371e50ae550d5a7cfee2c5d0d868e3678
Ashok Raju <deville75@gmail.com>
2021-09-19 09:12:53 -0600

    Add styled and chakra button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,29 @@
+import { chakra } from '@chakra-ui/system';
 import type { NextPage } from 'next'
+import styled from 'styled-components';
 
+const StyledButton = styled.div`
+  width: fit-content;
+  padding: 6px 8px;
+  margin: 4px;
+  background-color: white;
+  border: 1px solid gray;
+  border-radius: 5px;
+`;
+
+const ChakraButton = chakra(StyledButton, {
+  baseStyle: {
+    backgroundColor: "lightblue",
+    border: "4px dashed blue",
+  }
+})
 
 const Home: NextPage = () => {
   return (
     <div>
       Next.js, Chakra and Styled Components
+      <StyledButton>Styled</StyledButton>
+      <ChakraButton>Chakra</ChakraButton>
     </div>
   )
 }
